89a3bc118cc2661037039de53b4965b5e688e6ad
John Kim <kakwak123@gmail.com>
2021-04-17 16:02:39 +1000

    add home page

--- app/src/screens/Home/index.js
+++ app/src/screens/Home/index.js
@@ -6,8 +6,15 @@ const Home = () => (
     <div className={styles.content}>
       <div className={styles.dividerLeft}>
         <h1 className={styles.title}>Connect. Exchange. Learn.</h1>
+        <h2 className={styles.subtitle}>
+          Connect with people from different background,
+        </h2>
+        <h2 className={styles.subtitle}>
+          Share your culture, Learn new languages.
+        </h2>
+        <button className={styles.button}>Get started</button>
       </div>
-      <div className={styles.dividerRight}>asdfasdf</div>
+      <div className={styles.dividerRight}></div>
     </div>
   </div>
 );
