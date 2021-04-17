4ebd2a254357b5cab769df4999d5c7ce699fec6d
John Kim <kakwak123@gmail.com>
2021-04-17 19:06:32 +1000

    added defualt header

--- app/src/components/header/index.js
+++ app/src/components/header/index.js
@@ -1,10 +1,36 @@
 import React from "react";
 import stylesheet from "./header.module.scss";
 import logoUrl from "../../media/hackathonlogo.png";
-const Header = () => (
-  <div className={stylesheet.header}>
-    <img className={stylesheet.logoImage} src={logoUrl} alt="LogoImage" />
-    <h1 className={stylesheet.logoText}>CrossChat</h1>
-  </div>
-);
+const Header = () => {
+  return (
+    <div className={stylesheet.header}>
+      <div className={stylesheet.logoContainer}>
+        <img className={stylesheet.logoImage} src={logoUrl} alt="LogoImage" />
+        <h1 className={stylesheet.logoText}>CrossChat</h1>
+      </div>
+      <ul className={stylesheet.headerList}>
+        <li>
+          <a className={stylesheet.hyperlinkList} href="/">
+            Home
+          </a>
+        </li>
+        <li>
+          <a className={stylesheet.hyperlinkList} href="/ContactUs">
+            Contact Us
+          </a>
+        </li>
+        <li>
+          <a className={stylesheet.hyperlinkList} href="/About">
+            About
+          </a>
+        </li>
+        <li>
+          <a className={stylesheet.hyperlinkList} href="/Login">
+            Login/Sign up
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 export default Header;
