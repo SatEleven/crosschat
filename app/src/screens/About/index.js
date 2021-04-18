f0deabfc7504cc236ec4d14d52a66d5056d5276a
John Kim <kakwak123@gmail.com>
2021-04-18 13:48:20 +1000

    added about us

--- app/src/screens/About/index.js
+++ app/src/screens/About/index.js
@@ -5,8 +5,22 @@ const About = () => {
   return (
     <div className={styles.content}>
       <div>
+        <h2>Connect. Exchange. Learn.</h2>
         <h1>About</h1>
-        <p>blah blah blah :)</p>
+
+        <p>
+          CrossChat is a great way to meet new international friends and
+          exchange each other's culture and language.
+        </p>
+        <p>
+          This service provides features to fix each other's sentences and have
+          translation service in case you don't understand it properly.{" "}
+        </p>
+        <p>
+          If you enjoy meeting awesome people, and like to learn their culture.
+        </p>
+        <p>You have came to the right place!</p>
+        <p> Sign up and start CrossChating!</p>
       </div>
     </div>
   );
