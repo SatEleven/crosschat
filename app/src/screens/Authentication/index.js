328d76ecc0af1ed78d1dfef547b0e729cef01656
John Kim <kakwak123@gmail.com>
2021-04-18 14:12:02 +1000

    added input field

--- app/src/screens/Authentication/index.js
+++ app/src/screens/Authentication/index.js
@@ -2,30 +2,29 @@ import styles from "./Authentication.module.scss";
 import { useHistory } from "react-router-dom";
 
 const Authentication = () => {
-    const history = useHistory();
-    const goLogin = () => history.push('/authentication');
-    const goNext = () => history.push('/forgotPassword')
+  const history = useHistory();
+  const goLogin = () => history.push("/authentication");
+  const goNext = () => history.push("/forgotPassword");
 
-    return(
-        <div className = {styles.container}>
-            <div className={styles.formInterestBox}>
-                <div className = {styles.formInterestForm}>
-                    <div className={styles.formEnterBox}>
-                        <p>Please enter authentication code sent to email</p>
-                        <div></div>
-                        <button className = {styles.editButton} onClick = {goNext}>Next</button>
-                        <a href = {goLogin}>Click if code was not received</a>
-
-
-                    </div>
-
-                </div>
-                    
+  return (
+    <div className={styles.container}>
+      <div className={styles.formInterestBox}>
+        <div className={styles.formInterestForm}>
+          <form className={styles.formEnterBox}>
+            <p>Please enter authentication code sent to email</p>
+            <div>
+              <input></input>
             </div>
+
+            <button className={styles.editButton} onClick={goNext}>
+              Next
+            </button>
+            <a href={goLogin}>Click if code was not received</a>
+          </form>
         </div>
-
-
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Authentication;
